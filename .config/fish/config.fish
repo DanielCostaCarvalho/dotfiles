85e702cf1f6c9de0c54dc6aee5762bcf27c2cacb
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2024-06-20 18:32:09 -0300

    feat: add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+set fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    atuin init fish | source
+end
+
+starship init fish | source
+~/.local/bin/mise activate fish | source
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -249,6 +249,40 @@ require("lazy").setup({
 
   -- key bindings
   { "folke/which-key.nvim",  config = true },
+
+  -- Debugger
+  -- {
+  --   "rcarriga/nvim-dap-ui",
+  --   dependencies = {
+  --     "mfussenegger/nvim-dap",
+  --     "nvim-neotest/nvim-nio"
+  --   },
+  --   config = function()
+  --     local dap, dapui = require("dap"), require("dapui")
+  --
+  --     dap.setup()
+  --     dapui.setup()
+  --
+  --     dap.adapters.php = {
+  --       type = "executable",
+  --       command = "node",
+  --
+  --     }
+  --
+  --     dap.listeners.before.attach.dapui_config = function()
+  --       dapui.open()
+  --     end
+  --     dap.listeners.before.launch.dapui_config = function()
+  --       dapui.open()
+  --     end
+  --     dap.listeners.before.event_terminated.dapui_config = function()
+  --       dapui.close()
+  --     end
+  --     dap.listeners.before.event_exited.dapui_config = function()
+  --       dapui.close()
+  --     end
+  --   end
+  -- }
 })
 
 vim.cmd.colorscheme("catppuccin-latte")
